cee8d4992d0b9e7ce916ba9fc00dddd8198eff8f
Benoît <benoit.boure@gmail.com>
2021-01-11 19:37:15 +0100

    Cleanup

--- packages/api/functions/express/lambda.js
+++
@@ -1,8 +0,0 @@
-import 'source-map-support/register'
-import serverlessExpress from '@vendia/serverless-express'
-import app from './app'
-
-const binaryMimeTypes = []
-const server = serverlessExpress.createServer(app, null, binaryMimeTypes)
-
-export const handler = (event, context) => serverlessExpress.proxy(server, event, context)
--- packages/api/functions/express/wrap-async.js
+++
@@ -1,3 +0,0 @@
-const wrapAsync = fn => (req, res, next) => fn(req, res, next).catch(next)
-
-export default wrapAsync
