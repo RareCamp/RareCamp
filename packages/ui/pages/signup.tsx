397acf625f2c018641b4ce310199145a4ae329a0
Brett <brett@vendia.net>
2021-02-11 20:16:10 +1000

    fix(ui): comment out broken placeholder code

--- packages/ui/pages/signup.tsx
+++ packages/ui/pages/signup.tsx
@@ -16,16 +16,16 @@ const SignUp = () => {
 
   async function signup () {
       try {
-          const { user } = await Auth.signUp({
-              username,
-              password,
-              attributes: {
-                  email,          // optional
-                  phone_number,   // optional - E.164 number convention
-                  // other custom attributes 
-              }
-          });
-          console.log(user);
+          // const { user } = await Auth.signUp({
+          //     username,
+          //     password,
+          //     attributes: {
+          //         email,          // optional
+          //         phone_number,   // optional - E.164 number convention
+          //         // other custom attributes 
+          //     }
+          // });
+          // console.log(user);
       } catch (error) {
           console.log('error signing up:', error);
       }
