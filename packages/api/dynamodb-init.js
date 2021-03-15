dab26491fe1323fb35288080dd67d3b3875da67f
Brett Andrews <brett.j.andrews@gmail.com>
2021-03-15 10:22:07 +1000

    fix: replace AWS_PROFILE with hardcoded rarecamp_dev (#53)
    * fix(ui): pass through all props from MyAppWrapper
    
    fixes TS error
    
    * fix: replace AWS_PROFILE with hardcoded rarecamp_dev
    
    To prevent accidental deployment to unintended AWS account

--- packages/api/dynamodb-init.js
+++ packages/api/dynamodb-init.js
@@ -5,13 +5,13 @@ import DynamoDB from 'aws-sdk/clients/dynamodb'
 // const { fromIni } = require("@aws-sdk/credential-provider-ini");
 
 const {
-  AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, NODE_ENV, AWS_PROFILE,
+  AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, NODE_ENV,
 } = process.env
 
 // For local development, get creds from ~/.aws/credentials
 // Alternatively, set AWS_PROFILE env var
 if (NODE_ENV !== 'test' && (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY)) {
-  const credentials = new AWS.SharedIniFileCredentials({ profile: AWS_PROFILE })
+  const credentials = new AWS.SharedIniFileCredentials({ profile: 'rarecamp_dev' })
   AWS.config.credentials = credentials
   // TODO: V3
   // const s3Client = new S3.S3Client({
