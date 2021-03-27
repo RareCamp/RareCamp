57787ac402736427f54463f36a890b369c16ff11
khalid ghiboub <kghiboub@gmail.com>
2021-03-27 09:14:41 +0400

    remove comment

--- packages/ui/context/workspace.tsx
+++ packages/ui/context/workspace.tsx
@@ -1,7 +1,6 @@
 import { createContext, useContext } from 'react';
 import {Workspace} from '../types'
 
-// user default workspace Context
 export const WorkspaceContext = createContext<Workspace[]>([]);
 
 export function useWorkspaceContext() {
